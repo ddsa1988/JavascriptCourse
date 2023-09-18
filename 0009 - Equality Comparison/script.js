ee28206f0c578813700f039e4eeb0fe2e44a9ff2
Diego Alexandre <ddsa.1988@outlook.com>
2023-09-18 14:28:37 -0300

    commit

--- 0009 - Equality Comparison/script.js
+++ 0009 - Equality Comparison/script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-//Loose equality: performs type conversion when comparing two things
+//Loose equality: performs type coercion when comparing two things
 console.log(`'1' == 1: ${"1" == 1}`);
 console.log(`'0' == 0: ${"0" == 0}`);
 console.log(`'1' == true: ${"1" == true}`);
 console.log(`'0' == false: ${"0" == false}`);
 console.log();
 
-//Strict equality: doesn't perform type conversion when comparing two things
+//Strict equality: doesn't perform type coercion when comparing two things
 console.log(`'1' === 1: ${"1" === 1}`);
 console.log(`'0' === 0: ${"0" === 0}`);
 console.log(`'1' === true: ${"1" === true}`);
