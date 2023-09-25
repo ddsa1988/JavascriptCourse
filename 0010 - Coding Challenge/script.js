fbb99edaaa4a6371e7e41cbaa35cbf93cea3f571
Diego Alexandre <ddsa.1988@outlook.com>
2023-09-25 16:23:37 -0300

    commit

--- 0010 - Coding Challenge/script.js
+++ 0010 - Coding Challenge/script.js
@@ -5,11 +5,11 @@ import promptSync from "prompt-sync";
 const prompt = promptSync();
 const pointsTeamA = [];
 const pointsTeamB = [];
-const MAX_POINTS = 3;
+const NUMBER_OF_SCORES = 3;
 
 let counter = 0;
 
-while (counter < MAX_POINTS) {
+while (counter < NUMBER_OF_SCORES) {
     try {
         const score = Number(
             prompt(console.log(`Type the team A ${counter + 1}º score: `))
@@ -28,7 +28,7 @@ while (counter < MAX_POINTS) {
 
 counter = 0;
 
-while (counter < MAX_POINTS) {
+while (counter < NUMBER_OF_SCORES) {
     try {
         const score = Number(
             prompt(console.log(`Type the team B ${counter + 1}º score: `))
