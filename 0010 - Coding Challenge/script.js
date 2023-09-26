6d3af6cb0c56ccf20f108b20f4773264bc333c58
Diego Alexandre <ddsa.1988@outlook.com>
2023-09-25 23:57:31 -0300

    commit

--- 0010 - Coding Challenge/script.js
+++ 0010 - Coding Challenge/script.js
@@ -3,47 +3,56 @@
 import promptSync from "prompt-sync";
 
 const prompt = promptSync();
-const pointsTeamA = [];
-const pointsTeamB = [];
+const data = {};
+
+const NUMBER_OF_TEAMS = 2;
 const NUMBER_OF_SCORES = 3;
 
-let counter = 0;
+let counterTeams = 0;
+let counterScores = 0;
 
-while (counter < NUMBER_OF_SCORES) {
-    try {
-        const score = Number(
-            prompt(console.log(`Type the team A ${counter + 1}º score: `))
-        );
+while (counterTeams < NUMBER_OF_TEAMS) {
+    const name = prompt(
+        console.log(`Type the ${counterTeams + 1}º team's name: `)
+    ).trim();
 
-        if (!(Number.isInteger(score) && score > 0)) {
-            throw new Error("Invalid score.");
-        }
-
-        pointsTeamA.push(score);
-        counter++;
-    } catch (error) {
-        console.error(error.toString());
+    if (!(typeof name === "string" && name.length > 0)) {
+        console.log("Invalid name.");
+        continue;
     }
+
+    data[name] = {
+        scores: [],
+        avg: 0,
+    };
+
+    counterScores = 0;
+
+    while (counterScores < NUMBER_OF_SCORES) {
+        try {
+            const score = Number(
+                prompt(
+                    console.log(
+                        `Type the ${name} ${counterScores + 1}º score: `
+                    )
+                )
+            );
+
+            if (!(Number.isInteger(score) && score > 0)) {
+                throw new Error("Invalid score.");
+            }
+
+            data[name].scores.push(score);
+            data[name].avg += score;
+
+            counterScores++;
+        } catch (error) {
+            console.error(error.toString());
+        }
+    }
+
+    data[name].avg /= data[name].scores.length;
+    counterTeams++;
 }
 
-counter = 0;
-
-while (counter < NUMBER_OF_SCORES) {
-    try {
-        const score = Number(
-            prompt(console.log(`Type the team B ${counter + 1}º score: `))
-        );
-
-        if (!(Number.isInteger(score) && score > 0)) {
-            throw new Error("Invalid score.");
-        }
-
-        pointsTeamB.push(score);
-        counter++;
-    } catch (error) {
-        console.error(error.toString());
-    }
-}
-
-console.log(pointsTeamA);
-console.log(pointsTeamB);
+console.log(data);
